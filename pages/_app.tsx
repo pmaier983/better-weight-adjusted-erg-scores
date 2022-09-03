cba2c19be32d54390a67ee592306061e6177d5f2
Phillip ED Maier <pmaier983@gmail.com>
2022-09-03 18:22:31 -0400

    Get basic Calculation working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
-export default MyApp
+export default MyApp;
