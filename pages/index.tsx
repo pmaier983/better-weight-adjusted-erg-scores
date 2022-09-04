de9abdd36e83c8928a097625b6db40c343aad8a6
Phillip ED Maier <pmaier983@gmail.com>
2022-09-04 08:54:23 -0400

    Polish site

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,105 +44,126 @@ const SystemSelector = ({
 const Home: NextPage = () => {
   const [finalValue, setFinalValue] = useState<string | number>();
   return (
-    <div className={styles.pageStyles}>
-      <h1>Better Weight Adjusted Erg Scores!</h1>
-      <h3>Why should you use this weight adjusting formula?</h3>
-      <ul className={styles.reasonsList}>
-        <li>Calibrated for fours instead of eights like in C2</li>
-        <li>
-          More transparent in the reasoning behind the components of the formula
-        </li>
-        <li>It is less aggressive at lower weights</li>
-      </ul>
+    <div className={styles.pageContainer}>
+      <div className={styles.pageStyles}>
+        <h1 className={styles.title}>Better Weight Adjusted Erg Scores!</h1>
+        <h3>Why should you use this weight adjusting formula?</h3>
 
-      <form
-        className={styles.inputsContainer}
-        onSubmit={(e) => {
-          e.preventDefault();
-
-          const formTarget: any = e.target;
-
-          const isDistanceMetric =
-            formTarget?.distanceMetricActive?.id === "distanceMetricActive";
-          const isWeightMetric =
-            formTarget?.weightMetricActive?.id === "weightMetricActive";
-
-          const weightRaw = parseInt(formTarget?.weight.value, 10);
-          const distanceRaw = parseInt(formTarget?.distance.value, 10);
-
-          const distanceMetric = isDistanceMetric
-            ? distanceRaw
-            : distanceRaw * 1609.344;
-          const weightMetric = isWeightMetric
-            ? weightRaw
-            : weightRaw * 0.453592;
-
-          // This will break with any change to form order... don't do this.
-          const hours = parseInt(formTarget[5].value, 10);
-          const minutes = parseInt(formTarget[6].value, 10);
-          const seconds = parseInt(formTarget[7].value, 10);
-
-          if (0 === hours && 0 === minutes && 0 === seconds) {
-            setFinalValue("Input a Time!");
-            return;
-          }
-          const [resultMin, resultSec] = WeightAdjustment(
-            distanceMetric,
-            minutes + hours * 60,
-            seconds,
-            weightMetric
-          );
-
-          if (resultMin > 60) {
-            setFinalValue(
-              `${Math.floor(resultMin / 60)}:${resultMin % 60}:${resultSec}`
-            );
-          } else {
-            setFinalValue(`${resultMin}:${resultSec}`);
-          }
-        }}
-      >
-        <div className={styles.inputContainer}>
-          <h4>Distance</h4>
-          <NumericInput
-            min={1}
-            required
-            id="distance"
-            stepSize={1000}
-            majorStepSize={5000}
-          />
-          <SystemSelector
-            metricText="metric (meters)"
-            imperialText="imperial (miles)"
-            id="distance"
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <h4>Time</h4>
-          <TimePicker
-            precision={TimePrecision.SECOND}
-            selectAllOnFocus
-            showArrowButtons
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <h4>Weight</h4>
-          <NumericInput min={1} required id="weight" majorStepSize={10} />
-          <SystemSelector
-            metricText="metric (kg)"
-            imperialText="imperial (lb)"
-            id="weight"
-          />
-        </div>
-        <Button
-          intent={Intent.PRIMARY}
-          type="submit"
-          className={styles.submitButton}
+        <p className={styles.description}>
+          This weight adjustment formula is based on first-principles physics as
+          opposed to the observation-based C2 formula. It makes a number of
+          approximations in order to be computationally light, but it has been
+          validated against the C2 formula so should be fairly accurate.
+        </p>
+        <a
+          href="/WeightAdjustment.pdf"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Calculate
-        </Button>
-      </form>
-      <div className={styles.result}>{finalValue}</div>
+          Click this link to download the paper.
+        </a>
+
+        <form
+          className={styles.inputsContainer}
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            const formTarget: any = e.target;
+
+            // const isDistanceMetric =
+            //   formTarget?.distanceMetricActive?.id === "distanceMetricActive";
+            const isWeightMetric =
+              formTarget?.weightMetricActive?.id === "weightMetricActive";
+
+            const weightRaw = parseInt(formTarget?.weight.value, 10);
+            // const distanceRaw = parseInt(formTarget?.distance.value, 10);
+
+            // const distanceMetric = isDistanceMetric
+            //   ? distanceRaw
+            //   : distanceRaw * 1609.344;
+            const weightMetric = isWeightMetric
+              ? weightRaw
+              : weightRaw * 0.453592;
+
+            // This will break with any change to form order... don't do this.
+            const hours = parseInt(formTarget[0].value, 10);
+            const minutes = parseInt(formTarget[1].value, 10);
+            const seconds = parseInt(formTarget[2].value, 10);
+
+            if (0 === hours && 0 === minutes && 0 === seconds) {
+              setFinalValue("Input a Time!");
+              return;
+            }
+            const [resultMin, resultSec] = WeightAdjustment(
+              // distanceMetric,
+              1000,
+              minutes + hours * 60,
+              seconds,
+              weightMetric
+            );
+
+            const formattedSec =
+              resultSec.toString().length === 1 ? `0${resultSec}` : resultSec;
+
+            if (resultMin > 60) {
+              setFinalValue(
+                `${Math.floor(resultMin / 60)}:${
+                  resultMin % 60
+                }:${formattedSec}`
+              );
+            } else {
+              setFinalValue(`${resultMin}:${formattedSec}`);
+            }
+          }}
+        >
+          {/* <div className={styles.inputContainer}>
+            <h4>Distance</h4>
+            <NumericInput
+              min={1}
+              required
+              id="distance"
+              stepSize={1000}
+              majorStepSize={5000}
+            />
+            <SystemSelector
+              metricText="metric (meters)"
+              imperialText="imperial (miles)"
+              id="distance"
+            />
+          </div> */}
+          <div className={styles.inputContainer}>
+            <h4>Time</h4>
+            <TimePicker
+              precision={TimePrecision.SECOND}
+              selectAllOnFocus
+              showArrowButtons
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <h4>Weight</h4>
+            <NumericInput min={1} required id="weight" majorStepSize={10} />
+            <SystemSelector
+              metricText="metric (kg)"
+              imperialText="imperial (lb)"
+              id="weight"
+            />
+          </div>
+          <Button
+            intent={Intent.PRIMARY}
+            type="submit"
+            className={styles.submitButton}
+          >
+            Calculate
+          </Button>
+        </form>
+        <div className={styles.result}>{finalValue}</div>
+        <h5 className={styles.tinyDetails}>Algorithm by Cal</h5>
+
+        <h5 className={styles.tinyDetails}>
+          Code by {Math.random() > 0.5 ? "Phillip & Seth" : "Seth & Phillip"}
+        </h5>
+      </div>
     </div>
   );
 };
